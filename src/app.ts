f22c0fd6e63c45f4dccd5a1e4b21587151ccc52d
mominul <ulhouqemomin39@gmail.com>
2023-11-26 09:55:54 +0600

    updated server

--- src/app.ts
+++ src/app.ts
@@ -18,8 +18,8 @@ app.use(cors());
 app.use('/api/v1', router);
 
 const test = (req: Request, res: Response) => {
-  const a = 10;
-  res.send(a);
+
+  res.send("hello i am running");
 };
 
 app.get('/', test);
